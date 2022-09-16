0a0eba2e4052ea8cd5a8ae97e43d1b3c7250709e
Fernando Ventura <fernando98alvarado@outlook.com>
2022-09-15 21:47:22 -0600

    Hello World!

---
+++ src/main/java/org/striker25/HelloWorld.java
@@ -0,0 +1,8 @@
+package org.striker25; // path where the file is located
+
+public class Main { // Name of classes always follow the PascalCase convention.
+
+  public static void main(String[] args) {
+    System.out.println("Hello world!");
+  }
+}
